3619b6c7ab9ef87c144cd61bbbc856142d89e499
Nurul Islam <nishojib@gmail.com>
2023-06-07 18:51:20 -0400

    day02: completed

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
-import "advent_of_code/2015/day01"
+import "advent_of_code/2015/day02"
 
 func main() {
-	day01.Part1SolveProblem()
-	day01.Part2SolveProblem()
+	//day01.Part1SolveProblem()
+	//day01.Part2SolveProblem()
+
+	day02.Part1SolveProblem()
+	day02.Part2SolveProblem()
 }
